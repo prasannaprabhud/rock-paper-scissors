a33102486866e2a1daa1121014f66db5484dae68
prasannaprabhud <prabhuverse1@gmail.com>
2025-03-08 14:47:55 +0530

    This was my rock paper scissors Console version v1.0
    It was taken me 2 days to complete.
    - Day 1 : pseudocode  and logic stuff
    - Day 2 : The whole code was written
    - Day 3 : today, git commit day

---
+++ script.js
@@ -0,0 +1,90 @@
+
+        // variables 
+        let round = 0;
+        const totalRound = 5;    
+        let humanScore = 0;
+        let computerScore = 0;  
+        const choices = ['rock', 'paper', 'scissors'];
+        // console designs and output
+        console.log(`Round : ${round}`);
+        console.log('----SCORE----');
+        console.log(`Computer : ${computerScore}`);
+        console.log(`Human : ${humanScore}`);
+        console.log('----------------------');
+
+
+        // random number generator
+        function getRandom(){
+            let randomNumber =  Math.floor(Math.random() * 3);
+            return randomNumber;
+        }
+
+        // human choice
+        function getHumanChoice(){
+            const user = prompt(`Round ${round} : Enter your choices `, 'rock'); // 'rock paper scissors'
+            console.log('----------------------');
+            console.log(`user : ${user}`);
+            return user.toLowerCase();
+        }
+
+        // computer choice
+        function getComputerChoice(){
+            let computerChoice = choices[getRandom()];
+            return computerChoice;
+        }
+
+        // humanChoice, computerChoice
+        // logic
+        function playRound(humanChoice, comp){
+            
+            if( ( humanChoice === comp ) == true ){
+                console.log(`Round ${round} : Match draws, ${humanChoice} equals ${comp}.  `);
+            } else if ( humanChoice === 'rock' && comp === 'scissors' || 
+                        humanChoice === 'paper' && comp === 'rock'    || 
+                        humanChoice === 'scissors' && comp === 'paper'){
+                console.log(`Round ${round} : You won, ${humanChoice} beats ${comp}. `);
+                return humanScore++;
+            }else if (humanChoice === 'scissors' && comp === 'rock' ||
+                      humanChoice === 'rock' && comp === 'paper'    ||
+                      humanChoice === 'paper' && comp === 'scissors'){
+                 console.log(`Round ${round} : AI won, ${comp} beats ${humanChoice}. `);
+                 return computerScore++;
+            }else{
+                console.log(`try next time`);   
+            } 
+           
+        }
+        
+        // match round, score hoster
+        function play(){
+            round++;
+            console.log(`Round : ${round}`);
+            console.log(`Computer : ${computerScore}`);
+            console.log(`Human : ${humanScore}`);
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice();
+            playRound(humanSelection, computerSelection);
+            console.log('----------------------');
+        }
+
+        // looping the round
+        play();
+        play();
+        play();
+        play();
+        play();
+
+            // if 5 rounds finishes after shows the message
+            if(humanScore > computerScore){
+                console.log(`----*CONGRATULATIONS!!!----`);
+                console.log(`Computer : ${computerScore}`);
+                console.log(`Human : ${humanScore}`);
+                console.log(`You Won, Score is : ${humanScore}`);
+            }else if(computerScore > humanScore){
+                console.log(`Computer : ${computerScore}`);
+                console.log(`Human : ${humanScore}`);
+                console.log(`ai Won, Score is : ${computerScore}`);
+            }else{
+                console.log('Match Draws');
+            }
+       
